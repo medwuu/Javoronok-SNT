3ea95ffcdf1ab36dae0810df4716b202702d7e15
medwuu <aepegon@gmail.com>
2024-02-04 21:11:31 +0300

    фикс мобильной версии, отрегулированы z-index'ы

--- src/additional/burger.js
+++ src/additional/burger.js
@@ -1,6 +1,5 @@
 document.querySelector('.burger').addEventListener('click', function() {
     this.classList.toggle('burger-active');
     document.querySelector('.navigation').classList.toggle('navigation-open');
-    document.querySelector('.main').classList.toggle('hide-element');
-    document.querySelector('.footer').classList.toggle('hide-element');
+    document.querySelector('body').classList.toggle('stop-scrolling');
 })

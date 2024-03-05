e95c62ccb0e3a829b027397757c3a44012e6d0f8
medwuu <aepegon@gmail.com>
2024-03-06 00:48:33 +0300

    скрипт переноса фотографий

--- python/links_tranfer.py
+++ python/links_tranfer.py
@@ -20,7 +20,22 @@ def formatText(old_connect: sqlite3.Connection, new_connect: sqlite3.Connection,
             continue
         if one_news_text and os.getenv("WEBSITE_NAME") in one_news_text:
             try:
-                sql = f"UPDATE 'news' SET text = '{one_news[3].replace(os.getenv("WEBSITE_NAME"), '../src/old-data/')}' WHERE id = '{one_news[0]}'"
+                sql = f"UPDATE 'news' SET text = '{one_news[3].replace(os.getenv('WEBSITE_NAME'), '../src/old-data/')}' WHERE id = '{one_news[0]}'"
+                new_cursor.execute(sql)
+                new_connect.commit()
+            except Exception as error:
+                print("Что-то пошло не так!\n", error)
+                return False
+    return True
+
+def formatImg(old_connect: sqlite3.Connection, new_connect: sqlite3.Connection, new_cursor: sqlite3.Cursor) -> bool:
+    old_data = getOldData(old_connect)
+    for one_news in tqdm(old_data):
+        if not one_news[4]:
+            continue
+        if os.getenv("IMAGE_WEBSITE_NAME") in one_news[4]:
+            try:
+                sql = f"UPDATE 'news' SET img = '{one_news[4].replace(os.getenv('IMAGE_WEBSITE_NAME'), '../src/old-data/').split('?')[0]}' WHERE id = '{one_news[0]}'"
                 new_cursor.execute(sql)
                 new_connect.commit()
             except Exception as error:
@@ -37,6 +52,7 @@ def main():
     new_cursor = new_connect.cursor()
 
     formatText(old_connect, new_connect, new_cursor)
+    formatImg(old_connect, new_connect, new_cursor)
 
 if __name__ == '__main__':
     main()
